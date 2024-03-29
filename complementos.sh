2175e572c1e1eb003c5e63c212cb772bab8e375f
Redzkull <manuelrmg234@gmail.com>
2024-03-28 19:40:50 -0600

    V2 conf

---
+++ complementos.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo dnf install htop -y 
+sudo dnf install neofetch -y 
+sudo dnf install tilix -y 
+sudo dnf install gtkterm -y
+sudo dnf install putty -y
+
--- vimx.sh
+++ vimx.sh
@@ -10,5 +10,33 @@ else
     echo "Hubo un problema durante la instalación de Vim y Vim-X11. Por favor, verifica los errores e inténtalo nuevamente."
 fi
 
+mkdir ~/.vimrc 
 
+# Texto que deseas agregar al archivo ~/.vimrc
+
+config_text="set number
+syntax enable
+set noshowmode
+colorscheme desert
+nnoremap <C-s> :x!<CR>
+set clipboard=unnamedplus \"Para poder usar el copiar y pegar
+set mouse=a
+set relativenumber   \" Muestra números relativos
+set numberwidth=4    \" Ancho de columna para números de línea
+set mouse=r	\" Para interactuar con el raton en vim
+set noswapfile
+
+call plug#begin('~/.vim/plugged')
+
+Plug 'vim-airline/vim-airline'
+Plug 'momota/cisco.vim'
+
+call plug#end()
+"
+
+# Ruta al archivo ~/.vimrc
+vimrc_file=~/.vimrc
+
+# Agregar el texto al final del archivo ~/.vimrc
+echo "$config_text" >> "$vimrc_file"
 
